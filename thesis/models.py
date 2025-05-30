101e3ce935483b48a9f55a829338c2523bd23320
Balagurunadhaswamy <balagurunadhaswamy.ts@gmail.com>
2025-05-31 02:11:08 +0530

    changed int fields to text fields

--- thesis/models.py
+++ thesis/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 class ThesisQueryResult(models.Model):
     query = models.TextField()
-    query_key = models.TextField()
+    query_key = models.TextField(default='')
     query_id = models.CharField(max_length=255, unique=True)  # ID from ChromaDB
     created_at = models.DateTimeField(auto_now_add=True)
 
@@ -36,14 +36,14 @@ class ThesisCompanyProfile(models.Model):
     investors = models.TextField(blank=True, null=True)
     stock_info = models.TextField(blank=True, null=True)
     similar_companies = models.TextField(blank=True, null=True)
-    total_users = models.IntegerField(blank=True, null=True)
-    average_time_on_site = models.FloatField(blank=True, null=True)
-    organic_search = models.IntegerField(blank=True, null=True)
-    organic_social_visits = models.IntegerField(blank=True, null=True)
-    paid_search = models.IntegerField(blank=True, null=True)
-    mail_visits = models.IntegerField(blank=True, null=True)
-    traffic_rank = models.IntegerField(blank=True, null=True)
-    referral_visits = models.IntegerField(blank=True, null=True)
+    total_users = models.TextField(blank=True, null=True)
+    average_time_on_site = models.TextField(blank=True, null=True)
+    organic_search = models.TextField(blank=True, null=True)
+    organic_social_visits = models.TextField(blank=True, null=True)
+    paid_search = models.TextField(blank=True, null=True)
+    mail_visits = models.TextField(blank=True, null=True)
+    traffic_rank = models.TextField(blank=True, null=True)
+    referral_visits = models.TextField(blank=True, null=True)
     parent_website = models.URLField(blank=True, null=True)
     employees = models.TextField(blank=True, null=True)
     employee_count = models.CharField(max_length=50, blank=True, null=True)
@@ -53,8 +53,8 @@ class ThesisCompanyProfile(models.Model):
     ceo_rating = models.CharField(max_length=50, blank=True, null=True)
     linkedin_company_profile = models.URLField(blank=True, null=True)
     crunchbase_url = models.URLField(blank=True, null=True)
-    logo = models.ImageField(upload_to='logos/', blank=True, null=True)
-    image = models.ImageField(upload_to='company_images/', blank=True, null=True)
+    logo = models.TextField(blank=True, null=True)
+    image = models.TextField(blank=True, null=True)
     updates = models.TextField(blank=True, null=True)
     affiliated = models.CharField(max_length=255, blank=True, null=True)
     get_directions_url = models.URLField(blank=True, null=True)
@@ -65,10 +65,10 @@ class ThesisCompanyProfile(models.Model):
     sic_code = models.CharField(max_length=50, blank=True, null=True)
     name_sectors = models.CharField(max_length=255, blank=True, null=True)
     parent_industry_sectors = models.CharField(max_length=255, blank=True, null=True)
-    paid_social_visits = models.IntegerField(blank=True, null=True)
-    display_ad_visits = models.IntegerField(blank=True, null=True)
-    average_bounce_rate = models.FloatField(blank=True, null=True)
+    paid_social_visits = models.TextField(blank=True, null=True)
+    display_ad_visits = models.TextField(blank=True, null=True)
+    average_bounce_rate = models.TextField(blank=True, null=True)
     query_stats = models.JSONField(blank=True, null=True)
 
     def __str__(self):
-        return self.company_id
+        return self.company_id or "Unknown Company"
